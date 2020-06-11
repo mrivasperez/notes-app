7afc34fc7f6492c5017dd2efff973adcb9d3f297
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-06-10 22:13:20 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
+
+// Customer yargs version
+yargs.version('1.2.3');
+
+// Add notes
+// Remove Notes
+// Read individual notes
+// List notes
+// Help options for all above
+
+// 1. Add Notes
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log(`Title: ${argv.title} Body: ${argv.body}`);
+    }
+
+})
+
+
+// Create Remove Command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note.',
+    handler: function(){
+        console.log(chalk.bold('Removing a note!'));
+    }
+})
+
+// List command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes.',
+    handler: function(){
+        console.log(chalk.bold('List all of the notes!'));
+    }
+})
+
+// Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note.',
+    handler: function(){
+        console.log(chalk.bold('Read this note!'));
+    }
+})
+
+yargs.parse();
---
+++ utils.js
@@ -0,0 +1,8 @@
+// This file contains the rest of the application
+console.log('utils.js is connected');
+
+const add = function(a, b) {
+    return a + b;
+};
+
+module.exports = add;
